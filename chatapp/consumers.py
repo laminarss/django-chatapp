ac4ce97c1f17af636b1c71b80d9dfe6b4a7a4def
narsimha <narasimhak453@gmail.com>
2023-09-24 22:29:31 +0530

    add websocket request handling

---
+++ chatapp/consumers.py
@@ -0,0 +1,11 @@
+from channels.generic.websocket import WebsocketConsumer
+
+class ChatConsumer(WebsocketConsumer):
+  def connect(self):
+    self.accept()
+    
+  def disconnect(self, code):
+    pass
+  
+  def receive(self, text_data):
+    pass
--- project_chatapp/asgi.py
+++ project_chatapp/asgi.py
@@ -11,6 +11,16 @@ import os
 
 from django.core.asgi import get_asgi_application
 
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+
+from chatapp.urls import websocket_urlpatterns
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project_chatapp.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+  'http': get_asgi_application(),
+  'websocket': AuthMiddlewareStack(
+    URLRouter(websocket_urlpatterns)
+  )
+})
