0436a9932c1336622f2b11f39df69344bb96123b
narsimha <narasimhak453@gmail.com>
2023-09-28 22:22:30 +0530

    modify urls (chatapp)

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -8,9 +8,9 @@ websocket_urlpatterns = [
 ]
 
 urlpatterns = [
-  path('api/register/', view=views.register_user, name='register'),
-  path('api/login/', view=views.login_user, name='login'),
-  path('api/logout/', view=views.logout_user, name='logout'),
-  path('api/online-users/', view=views.get_online_users, name='online_users'),
-  path('api/chat/start/', view=views.start_chat, name='start_chat'),
+  path('register/', view=views.register_user, name='register'),
+  path('login/', view=views.login_user, name='login'),
+  path('logout/', view=views.logout_user, name='logout'),
+  path('online-users/', view=views.get_online_users, name='online_users'),
+  path('chat/start/', view=views.start_chat, name='start_chat'),
 ]
