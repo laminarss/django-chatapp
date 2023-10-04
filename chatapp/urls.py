ce93f4e305c5d423ac5aba22e292e65193b0440e
Narsimha <narasimhak453@gmail.com>
2023-10-04 11:48:21 +0530

    update urls

--- chatapp/urls.py
+++ chatapp/urls.py
@@ -11,6 +11,7 @@ urlpatterns = [
   path('', view=views.index, name='index'),
   path('register/', view=views.register_user, name='register'),
   path('login/', view=views.login_user, name='login'),
+  path('home/', view=views.home, name='home'),
   path('logout/', view=views.logout_user, name='logout'),
   path('online-users/', view=views.get_online_users, name='online_users'),
   path('chat/start/', view=views.start_chat, name='start_chat'),
